6df79480c86931e8bc8a8978b8caa14c98a9fdfc
christianxxr <christian.diez9558@gmail.com>
2022-12-01 18:01:25 +0100

    Funcionalidad empleado introducida Tarde 01/12 updated

--- src/app/components/nuevo-inmueble/nuevo-inmueble.component.ts
+++ src/app/components/nuevo-inmueble/nuevo-inmueble.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nuevo-inmueble',
   templateUrl: './nuevo-inmueble.component.html',
-  styleUrls: ['./nuevo-inmueble.component.css']
+  styleUrls: ['./nuevo-inmueble.component.css'],
 })
 export class NuevoInmuebleComponent implements OnInit {
+  ngDropdownOperacion: string | undefined;
 
-  constructor() { }
+  operacion = [
+    { value: 'alquiler', viewValue: 'Alquiler' },
+    { value: 'compra', viewValue: 'Compra' },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
