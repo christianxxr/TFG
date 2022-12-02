eaf4e917420cc2aa9fea9209eed99b000e841e49
Jesús <jd_rico@yahoo.es>
2022-12-02 13:46:27 +0100

    Solucionado CORS -> muestra inmuebles en consola

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { InmueblesService } from './servicios/inmuebles.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [InmueblesService]
 })
 export class AppComponent implements OnInit {
+
   title = 'FutureSweetHome';
+
+  constructor(private dataInmuebles: InmueblesService) {}
 
   ngOnInit(): void {
     console.log('Componente principal inicializado');
--- src/app/servicios/inmuebles.service.ts
+++ src/app/servicios/inmuebles.service.ts
@@ -8,12 +8,12 @@ import { IntInmueble } from '../model/inmuebles.interface';
 })
 export class InmueblesService {
 
-  urlRequestMapping: string = 'http://localhost:8089/inmuebles/';
+  //urlRequestMapping: string = 'http://localhost:8089/inmuebles/';
 
   constructor(private http: HttpClient) { }
 
   mostrarTodosLosInmuebles(): Observable<IntInmueble[]> {
-    let url = this.urlRequestMapping + 'mostrarTodos';
-    return this.http.get<IntInmueble[]>(url);
+    //let url = this.urlRequestMapping + 'mostrarTodos';
+    return this.http.get<IntInmueble[]>('/inmuebles/mostrarTodos');
   }
 }
