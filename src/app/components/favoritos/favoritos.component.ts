212ccb85c9732e492c643f0e8318002b893d08de
christianxxr <christian.diez9558@gmail.com>
2022-12-09 15:58:56 +0100

    favoritos alimentado

--- src/app/components/favoritos/favoritos.component.ts
+++ src/app/components/favoritos/favoritos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IntInmueble } from 'src/app/model/inmuebles.interface';
+import { InmueblesService } from 'src/app/servicios/inmuebles.service';
 
 @Component({
   selector: 'app-favoritos',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favoritos.component.css'],
 })
 export class FavoritosComponent implements OnInit {
-  numeros: any;
-  constructor() {
-    this.numeros = Array(50);
+  inmueble: IntInmueble = {
+    inmuebleId: '',
+    tipoOperacion: '',
+    pais: '',
+    zona: '',
+    area: '',
+    tipoInmueble: '',
+    precio: 0,
+    urlImagen: '',
+    guardadoPor: [],
+  };
+
+  listaInmuebles: IntInmueble[] = [];
+  constructor(private servicio: InmueblesService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.obtenerTodosLosInmuebles();
   }
 
-  ngOnInit(): void {}
+  obtenerTodosLosInmuebles() {
+    this.servicio.mostrarTodosLosInmuebles().subscribe((data) => {
+      this.listaInmuebles = data;
+    });
+  }
 }
